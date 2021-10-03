a3db3f0d7d97a9242b5863e1c88410f42a251a20
duckier <duckier@naver.com>
2021-10-03 14:55:05 +0900

    회원가입

--- index.js
+++ index.js
@@ -2,12 +2,38 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const config = require('./config/key');
+
+const { User } = require('./model/User')
+
+// application/x-www-form-urlendocoded
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://user:1qaz2wsx!%40@cluster0.gxx7t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send("Hello world!!! hi~"))
+
+
+
+app.get('/', (req, res) => res.send("Hello world!!!"))
+
+// 회원가입
+app.post('/register', (req, res) => {
+
+    const user = new User(req.body);
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+
+        return res.status(200).json({
+            sucess: true
+        })
+    })
+
+})
 
 app.listen(port, () => console.log(`Example app listening on poar ${port}!`))
